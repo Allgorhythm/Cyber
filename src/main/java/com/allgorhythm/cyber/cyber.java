019689f8240e0b97cc9ae543b325a6b5e74b9c96
Allgorhythm <allgorhythm.mail@gmail.com>
2015-05-09 16:04:04 -0700

    09-05-2015 Starting on the mod

--- src/main/java/com/allgorhythm/cyber/cyber.java
+++ src/main/java/com/allgorhythm/cyber/cyber.java
@@ -1,7 +1,38 @@
 package com.allgorhythm.cyber;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
 /**
  * Created by Brandon on 5/9/2015.
  */
-public class cyber {
-}
+
+    @Mod(modid = "cyber", name = "cyber", version = "1.7.2-1.0")
+    public class cyber
+    {
+
+        @Mod.Instance("cyber")
+        public static cyber instance;
+
+        @Mod.EventHandler
+        public void preinit (FMLPreInitializationEvent event)
+        {
+
+        }
+
+        @Mod.EventHandler
+        public void  init (FMLInitializationEvent event)
+        {
+
+        }
+
+        @Mod.EventHandler
+        public void postinit (FMLPostInitializationEvent event)
+        {
+
+        }
+    }
+
+
