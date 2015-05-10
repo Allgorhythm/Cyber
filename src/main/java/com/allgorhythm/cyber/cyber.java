50158ca8d89b0274ef17ad53fb0d97bd7f07daec
Allgorhythm <allgorhythm.mail@gmail.com>
2015-05-09 21:21:04 -0700

    09-05-2015 mcmodinfo file and Mod class java file added

--- src/main/java/com/allgorhythm/cyber/cyber.java
+++ src/main/java/com/allgorhythm/cyber/cyber.java
@@ -5,9 +5,6 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-/**
- * Created by Brandon on 5/9/2015.
- */
 
     @Mod(modid = "cyber", name = "Cyber()", version = "1.7.2-1.0")
     public class cyber
